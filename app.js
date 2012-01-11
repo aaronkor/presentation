ae01635d193cb08e5ca4feb1f71af91057782ce5
Aaron Kor <aaron.kor@gmail.com>
2012-01-10 16:03:03 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express.createServer();
+
+app.set('view engine', 'jade');
+app.set('view options', {
+  layout: false
+});
+
+app.use('/media', express.static(__dirname + '/media'));
+//app.use('/media/js', express.static(__dirname + 'media/js/'));
+
+app.get('/', function(req, res){
+  res.send('hello world\n' + __dirname + 'media/js/');
+});
+
+app.get('/slides', function(req, res){
+  res.render('slides');
+});
+
+app.listen(3000);
+console.log('Listening at http://localhost:3000 ...\n');
