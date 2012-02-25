0deab07063bd1a0cbeb26b9c3bb9b11480a7f79d
Aaron Kor <aaron.kor@gmail.com>
2012-02-25 09:14:58 -0800

    more elegand syntax for view

--- media/js/default.js
+++ media/js/default.js
@@ -21,87 +21,27 @@ impress.steps = {
         x: -200, y: 2500, scale: 4  },
     "king": {
         x: 3700, y: 500, scale: 0.5  },
-    "conventional-query": {
-        x: -600, y: 600, scale: 1 },
     "node-query": {
-        x: -200, y: 1400, scale: 4  },
+        x: -200, y: 1400, scale: 3  },
     "timeout-example": {
         x: 5900, y: -1000, scale: 1  },
     "write-code": {
         x: 2050, y: 1900, scale: 2.5, rotate: -90  },
     "cloudST-1": {
-        x: 500, y: 500, scale: 1.5 },
+        x: 1100, y: 200, scale: 1.5 },
     "cloudST-2": {
-        x: 500, y: 500, scale: 1.5 },
+        x: 1100, y: 200, scale: 1.5 },
     "npm": {
         x: 3500, y: 2350, scale: 1.5  },
     "packages": {
         x: 3450, y: 1300, scale: 1.5  },
     "sum-up": {
-        x: 4000, y: -100, scale: 1, rotate: -90  },
+        x: 4000, y: -100, scale: 2, rotate: -90  },
 
     "thanks": {
         x: 5500, y: 2150, scale: 3.5 },
-
-
-
-
-    "what-is-deferred": {
-        x: 100, y: 1270, scale: 4 },
-    "deferred": {
-        x: -800, y: 2000, scale: 1.5 },
-    "deferred-example": {
-        x: 700, y: 2000, scale: 1.5 },
-
-    "what-is-a-promise": {
-        x: 2800, y: 1000, scale: 4 },
-    "promise": {
-        x: 2200, y: 1650, scale: 1.5 },
-    "attaching-observers": {
-        x: 3500, y: 1550, scale: 1.5 },
-    "chaining-promises": {
-        x: 2200, y: 2600, scale: 1.5 },
-    "chaining-promises-cont": {
-        x: 2200, y: 3500, scale: 1.5 },
-    "nesting-promises": {
-        x: 2200, y: 4300, scale: 1.5 },
-    "error-handling": {
-        x: 3500, y: 2600, scale: 1.5 },
-    "end": {
-        x: 3500, y: 3800, scale: 1.5 },
-
-    "promisify": {
-        x: -800, y: 3100, scale: 1.5 },
-    "promisify-cont": {
-        x: -800, y: 4000, scale: 1.5 },
-    "grouping-promises": {
-        x: 700, y: 2950, scale: 1.5 },
-    "processing-collections": {
-        x: 700, y: 3850, scale: 1.5 },
-    "promise-extensions": {
-        x: -800, y: 4800, scale: 1.5 },
-    "invoke": {
-        x: 700, y: 5000, scale: 1.5 },
-
-    "match": {
-        x: 2200, y: 5100, scale: 1.5 },
-    "example-promises": {
-        x: 3500, y: 5000, scale: 1.5 },
-
-    "future": {
-        x: 4900, y: 3800, scale: 1.5 },
-    "coroutines": {
-        x: 6200, y: 3800, scale: 1.5 },
-    "proxies": {
-        x: 7500, y: 3700, scale: 1.5 },
-    "example-proxies": {
-        x: 7500, y: 4650, scale: 1.5 },
-
-    "questions": {
-        x: 8000, y: 2000, rotate: { y: 65, z: -90 }, scale: 5 },
-
-    "thank-you": {
-        x: 6400, y: 1600, scale: 5 }
+    "overview": {
+        x:2500, y:1000, scale:10 }
 };
 //if (location.pathname.match(/\/3d\/(?:index\.html)?$/)) {
 //    document.getElementById('fm1').className = 'fallback-message';
